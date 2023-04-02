aad717dbd4a7b41aafd257f3020a6f6e88f9e6d2
Acumane <94324138+Acumane@users.noreply.github.com>
2023-04-02 04:03:09 -0400

    Prioritize commits to 'main,' branch selection

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from datetime import datetime as Date
 import datetime as Time
 
 console = Console()
-owner = repo = user = ""; days = "0"
+owner = repo = user = branch = ""; days = "0"
 done = False
 
 console.print("[bold] :clipboard: Commitment [/]", justify="center")
@@ -15,16 +15,17 @@ while(not done):
     console.print("[strike] [/]" * console.width)
     if path.exists(".hist") and path.getsize(".hist"): 
         with open(".hist", 'r') as hist:
-            saved = hist.read().split()
+            saved = hist.read().splitlines()
             console.print(f"Press enter to use a previous entry   [ {' | '.join(saved)} ]\n", style="#808080", highlight=False)
-            owner, repo, user, days = saved
+            owner, repo, branch, user, days = saved
 
     owner = console.input("Repo owner or org name: [red]") or owner
     repo = console.input("Repo name: [red]") or repo
-    user = console.input("[default not bold]Your Git(Hub) username:[/] ") or user
+    branch = console.input("Which branches? [red]") or branch
+    user = console.input("[default not bold]Git(Hub) username:[/] ") or user
     days = console.input("Commits up to [bold cyan]x[/] days ago [#808080](* for all)[/]: ") or days
 
-    if "" in [owner, repo, user, days]:
+    if "" in [owner, repo, branch, user, days]:
         console.print("\n:exclamation: Insufficient information provided! Retry:\n", style="red")
         continue
 
@@ -32,17 +33,22 @@ while(not done):
         console.print("\n:exclamation: Input is invalid! Retry:\n", style="red")
         continue
 
-    url =  f"https://api.github.com/repos/{owner}/{repo}/branches"
+    url =  f"https://api.github.com/repos/{owner}/{repo}/branches?per_page=100"
     response = get(url).json()
 
-    if "message" in response and response["message"] == "Not Found":
-        console.print("\n:exclamation: Repo is either private or nonexistant! Retry:\n", style="red")
-        continue
+    if "message" in response:
+        if response["message"] == "Not Found":
+            console.print("\n:exclamation: Repo is either private or nonexistant! Retry:\n", style="red")
+            continue
+        if response["message"].contains("API rate limit exceeded"):
+            console.print("\n:exclamation: GitHub API rate limit exceeded!\n", style="red")
+            continue
 
     with open('.hist', 'w') as hist:
-        hist.write(owner + " ")
-        hist.write(repo + " ")
-        hist.write(user + " ")
+        hist.write(owner + "\n")
+        hist.write(repo + "\n")
+        hist.write(branch + "\n")
+        hist.write(user + "\n")
         hist.write(days)
 
     class Branch:
@@ -50,15 +56,9 @@ while(not done):
             self.name = n
             self.commits = c
 
-    branches = []
-    all_commits = set()
-    days_ago = 0 if days == '*' else int(days)
-    count = 0
-    for b in reversed(response): # oldest -> newest branch
+    def get_commits(name, link):
         commits = []   
-        sha = b["commit"]["sha"]
-        url =  f"https://api.github.com/repos/{owner}/{repo}/commits?per_page=100&sha={sha}"
-        response = get(url).json()
+        response = get(link).json()
         cutoff = Date.utcnow() - Time.timedelta(days=days_ago)
         
         for c in response:
@@ -71,13 +71,32 @@ while(not done):
                         commits.append(c["html_url"])
 
         all_commits.update(commits)
-        branches.append( Branch(b["name"], commits) )
+        branches.append(Branch(name, commits))
+
+    url = f"https://api.github.com/repos/{owner}/{repo}"
+    default = get(url).json()["default_branch"]
+
+    branch_list = branch.split(", ")
+    branches, links, all_commits = [], {}, set()
+    days_ago = 0 if days == '*' else int(days)
+    for branch in response:
+        name = branch["name"]
+        if name not in branch_list: continue
+        sha = branch["commit"]["sha"]
+        url = f"https://api.github.com/repos/{owner}/{repo}/commits?per_page=100&sha={sha}"
+        links[name] = url
+
+    get_commits(default, links[default])
+
+    for l in links.items():
+        if l[0] != default:
+            get_commits(l[0], l[1])
 
     qualify = f" in the past {str(days_ago) + ' days' if days_ago > 1 else 'day'}"
     if len(all_commits):
         print(f"\n{user} made {len(all_commits)} commit{'s' if len(all_commits) > 1 else ''} to {owner}/{repo}{qualify if days_ago else ''}:")
-        for B in reversed(branches):
-            if B.commits: print(f"\n[bold]\u2387[/bold] {B.name}")
+        for B in branches:
+            if B.commits: print(f"\n[bold]\u2387[/bold] {B.name} {'(Default)' if B.name == default else ''}")
             for c in B.commits:
                 print(f"[link]{c}[/link]")
     else:
